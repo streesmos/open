a6151357f45e83be595604db7004ecd2183683a3
streesmos <61740996+streesmos@users.noreply.github.com>
2025-07-24 00:17:35 +0300

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -10,7 +10,7 @@ export default function HeroHome() {
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-600),var(--color-indigo-600),var(--color-gray-600),var(--color-indigo-900),var(--color-gray-600))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
               Продукция «АЛМ» для гидроизоляции и ремонта
@@ -51,15 +51,7 @@ export default function HeroHome() {
             </div>
           </div>
 
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={1104}
-            thumbHeight={576}
-            thumbAlt="Modal video thumbnail"
-            video="videos//video.mp4"
-            videoWidth={1920}
-            videoHeight={1080}
-          />
+        
         </div>
       </div>
     </section>
